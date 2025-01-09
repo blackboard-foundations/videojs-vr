208250647287d18bf3621b0f5b7aa40ed45b7fb2
Simon Gaeremynck <simon.gaeremynck@anthology.com>
2025-01-09 14:22:19 +0000

    chore: threejs should be a regular dependency, not a peer dependency

--- scripts/rollup.config.js
+++ scripts/rollup.config.js
@@ -2,37 +2,8 @@ const generate = require('videojs-generate-rollup-config');
 
 // see https://github.com/videojs/videojs-generate-rollup-config
 // for options
+const config = generate({});
 
-// Don't bundle three.js with the plugin
-const config = generate({
-  externals(defaults) {
-    return {
-      browser: defaults.browser.concat([
-        'three',
-        'THREE'
-      ]),
-      module: defaults.module.concat([
-        'three',
-        'THREE'
-      ]),
-      test: defaults.test.concat([
-        'three',
-        'THREE'
-      ])
-    };
-  }
-});
-
-config.builds.module.plugins = config.builds.module.plugins || [];
-config.builds.module.plugins.push({
-  name: 'exclude-threejs',
-  resolveId(source) {
-    if (source === 'three' || source === 'THREE') {
-      return { id: source, external: true };
-    }
-    return null;
-  }
-});
 // Add additional builds/customization here!
 
 // export the builds to rollup
